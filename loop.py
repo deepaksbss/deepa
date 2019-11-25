d08625e04734b1d8057b1fdbbaa100370f738764
deepaksbss <57767868+deepaksbss@users.noreply.github.com>
2019-11-25 23:42:14 +0530

    basic of python program

---
+++ loop.py
@@ -0,0 +1,21 @@
+sum = 0;
+num = int(input("Enter the values:"))
+end = int(input("Enter the end values"))
+#step = int(input("Enter the step values"))
+for i in range(num, end):
+    
+    #print("you enter the values".format(i))
+    #print(i * i)
+ #   print(i);
+  #  print()
+     #sum = sum + i;
+     #if i % 2 == 0:
+      #   print("This is Even number{}".format(i))
+
+     #else:
+      #   print("This is Odd number{}".format(i))
+    # print(i)
+#else:
+   # print("The loop as been completed")
+    
+   
